6ff8d89e98a407c015c34331c38495b337564d91
James <mirageapache@gmail.com>
2024-09-23 23:04:02 +0800

    Update portfolio data

--- src/assets/dummyData/portfolio_data.js
+++ src/assets/dummyData/portfolio_data.js
@@ -1,48 +1,75 @@
-import simple_twitter from 'assets/project_img/simple_twitter.png';
-import github_issue from 'assets/project_img/github_issue.jpg';
-import my_portfolio from 'assets/project_img/portfolio.jpg';
-import simple_dashboard from 'assets/project_img/simple_dashboard.jpg';
-import live_chat_app from 'assets/project_img/live_chat_app.jpg';
+import simple_twitter from "assets/project_img/simple_twitter.png";
+import github_issue from "assets/project_img/github_issue.jpg";
+import my_portfolio from "assets/project_img/portfolio.jpg";
+import simple_dashboard from "assets/project_img/simple_dashboard.jpg";
+import live_chat_app from "assets/project_img/live_chat_app.jpg";
+import blog_system from "assets/project_img/blog_system.png";
 
 export const portfolio_data = [
   {
-    'image': my_portfolio,
-    'title': 'My Portfolio',
-    'content': '個人網站，使用 React 框架及 RWD 模式開發，並使用Github Pages部署。',
-    'link': 'https://github.com/mirageapache/portfolio',
-    'demo_link': 'https://mirageapache.github.io/portfolio/',
-    'tag': ['React', 'SCSS', 'Github Pages']
+    image: my_portfolio,
+    title: "My Portfolio",
+    content:
+      "個人網站，使用 React 框架及 RWD 模式開發，並使用Github Pages部署。",
+    link: "https://github.com/mirageapache/portfolio",
+    demo_link: "https://mirageapache.github.io/portfolio/",
+    tag: ["React", "SCSS", "Github Pages"],
   },
   {
-    'image': simple_twitter,
-    'title': 'Simple Twitter',
-    'content': 'AC的畢業專題，與組員一起完成一個簡易的社群平台系統。',
-    'link': 'https://github.com/mirageapache/simple_twitter_frontend',
-    'demo_link': '',
-    'tag': ['React', 'axios', 'Github Pages','多人協作']
+    image: blog_system,
+    title: "Blog System",
+    content: "參考Substack平台構想並實作的部落格系統，全端開發。",
+    link: "https://github.com/mirageapache/BlogSystem_FE",
+    demo_link: "https://blog-system-fe.vercel.app/",
+    tag: [
+      "React",
+      "Tailwind",
+      "TypeScript",
+      "Redux",
+      "ReactQuery",
+      "Express",
+      "MongoDB",
+    ],
   },
   {
-    'image': github_issue,
-    'title': 'Github Issue',
-    'content': '使用React開發並透過Express串接Github提供的API，可以簡易的操作個人Github Issue。',
-    'link': 'https://github.com/mirageapache/github_issue_client',
-    'demo_link': 'https://mirageapache.github.io/github_issue_client',
-    'tag': ['React', 'SCSS', 'Bootstrap', 'axios', 'Express']
+    image: simple_twitter,
+    title: "Simple Twitter",
+    content: "AC的畢業專題，與組員一起完成一個簡易的社群平台系統。",
+    link: "https://github.com/mirageapache/simple_twitter_frontend",
+    demo_link: "",
+    tag: ["React", "axios", "Github Pages", "多人協作"],
   },
   {
-    'image': simple_dashboard,
-    'title': 'Simple Dashboard',
-    'content': '簡易的戶口統計網站，串接政府資料開放平臺所提供的API資料，並以圖表型式呈現。',
-    'link': 'https://github.com/mirageapache/Simple_Dashboard',
-    'demo_link': 'https://mirageapache.github.io/Simple_Dashboard',
-    'tag': ['React', 'SCSS', 'axios', 'TypeScript', 'Highcharts']
+    image: github_issue,
+    title: "Github Issue",
+    content:
+      "使用React開發並透過Express串接Github提供的API，可以簡易的操作個人Github Issue。",
+    link: "https://github.com/mirageapache/github_issue_client",
+    demo_link: "https://mirageapache.github.io/github_issue_client",
+    tag: ["React", "SCSS", "Bootstrap", "axios", "Express"],
   },
   {
-    'image': live_chat_app,
-    'title': 'Live Chat',
-    'content': '使用 React 框架搭配 Socket.io 套件，製作線上聊天室系統。',
-    'link': 'https://github.com/mirageapache/Socketio_chat_app',
-    'demo_link': 'https://mirageapache.github.io/Socketio_chat_app/',
-    'tag': ['React', 'SCSS', 'TypeScript', 'Socket.io', 'Redux Toolkit', 'Express']
+    image: simple_dashboard,
+    title: "Simple Dashboard",
+    content:
+      "簡易的戶口統計網站，串接政府資料開放平臺所提供的API資料，並以圖表型式呈現。",
+    link: "https://github.com/mirageapache/Simple_Dashboard",
+    demo_link: "https://mirageapache.github.io/Simple_Dashboard",
+    tag: ["React", "SCSS", "axios", "TypeScript", "Highcharts"],
   },
-]
+  {
+    image: live_chat_app,
+    title: "Live Chat",
+    content: "使用 React 框架搭配 Socket.io 套件，製作線上聊天室系統。",
+    link: "https://github.com/mirageapache/Socketio_chat_app",
+    demo_link: "https://mirageapache.github.io/Socketio_chat_app/",
+    tag: [
+      "React",
+      "SCSS",
+      "TypeScript",
+      "Socket.io",
+      "Redux Toolkit",
+      "Express",
+    ],
+  },
+];
