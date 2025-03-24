38cc287c52d174ec5d707d1d861bb4649487e9f1
jameswang14 <james.wang@settour.com.tw>
2025-03-24 11:09:15 +0800

    update portfolio data

--- src/assets/dummyData/portfolio_data.js
+++ src/assets/dummyData/portfolio_data.js
@@ -1,20 +1,26 @@
 import simple_twitter from "assets/project_img/simple_twitter.png";
 import github_issue from "assets/project_img/github_issue.jpg";
 import my_portfolio from "assets/project_img/portfolio.jpg";
-import simple_dashboard from "assets/project_img/simple_dashboard.jpg";
+import fire_message from "assets/project_img/fire_message.png";
 import live_chat_app from "assets/project_img/live_chat_app.jpg";
 import blog_system from "assets/project_img/blog_system.png";
 
 export const portfolio_data = [
   {
-    image: my_portfolio,
-    id: 'my_portfolio',
-    title: "My Portfolio",
-    content:
-      "個人網站，使用 React 框架及 RWD 模式開發，並使用Github Pages部署。",
-    link: "https://github.com/mirageapache/portfolio",
-    demo_link: "https://mirageapache.github.io/portfolio/",
-    tag: ["React", "SCSS", "RWD", "Github Pages"],
+    image: fire_message,
+    id: "fire_message",
+    title: "Fire Message",
+    content: "使用Next搭配Firebase開發的即時聊天室系統。",
+    link: "https://github.com/mirageapache/FireMessage",
+    demo_link: "https://fire-message.vercel.app/",
+    tag: [
+      "Next",
+      "Tailwind",
+      "TypeScript",
+      "Redux",
+      "Firebase",
+      "RWD",
+    ],
   },
   {
     image: blog_system,
@@ -35,6 +41,16 @@ export const portfolio_data = [
     ],
   },
   {
+    image: my_portfolio,
+    id: 'my_portfolio',
+    title: "My Portfolio",
+    content:
+      "個人網站，使用 React 框架及 RWD 模式開發，並使用Github Pages部署。",
+    link: "https://github.com/mirageapache/portfolio",
+    demo_link: "https://mirageapache.github.io/portfolio/",
+    tag: ["React", "SCSS", "RWD", "Github Pages"],
+  },
+  {
     image: simple_twitter,
     id: "simple_twitter",
     title: "Simple Twitter",
@@ -53,16 +69,16 @@ export const portfolio_data = [
     demo_link: "https://mirageapache.github.io/github_issue_client",
     tag: ["React", "SCSS", "Bootstrap", "axios", "Express"],
   },
-  {
-    image: simple_dashboard,
-    id: "simple_dashboard",
-    title: "Simple Dashboard",
-    content:
-      "簡易的戶口統計網站，串接政府資料開放平臺所提供的API資料，並以圖表型式呈現。",
-    link: "https://github.com/mirageapache/Simple_Dashboard",
-    demo_link: "https://mirageapache.github.io/Simple_Dashboard",
-    tag: ["React", "SCSS", "axios", "TypeScript", "Highcharts"],
-  },
+  // {
+  //   image: simple_dashboard,
+  //   id: "simple_dashboard",
+  //   title: "Simple Dashboard",
+  //   content:
+  //     "簡易的戶口統計網站，串接政府資料開放平臺所提供的API資料，並以圖表型式呈現。",
+  //   link: "https://github.com/mirageapache/Simple_Dashboard",
+  //   demo_link: "https://mirageapache.github.io/Simple_Dashboard",
+  //   tag: ["React", "SCSS", "axios", "TypeScript", "Highcharts"],
+  // },
   {
     image: live_chat_app,
     id: "live_chat_app",
